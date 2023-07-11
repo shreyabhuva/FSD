2c9c2e611ad6c9830bd09953b75ff0f9505886bf
shreya bhuva <138491142+shreyabhuva@users.noreply.github.com>
2023-07-11 10:20:37 +0530

    prac2 completed

---
+++ prac2/src/App.js
@@ -0,0 +1,24 @@
+//import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+function App() {
+  return(
+  <div className='wrapper'>
+    <h1>Marine Mammals</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route exact path='/' element={<Home/>}/>
+      <Route exact path='/contact' element={<Contact/>}/>
+      <Route exact path='/about' element={<About/>}/>
+    </Routes>
+    </BrowserRouter>
+  </div>
+  )
+}
+
+
+export default App;
